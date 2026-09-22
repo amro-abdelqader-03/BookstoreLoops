812792de698cd6c33274dead183c523ae7767717
amro-abdelqader-03 <amro.abdelqader.03@gmail.com>
2026-09-22 21:16:30 +0300

    add Q13

---
+++ exercises/Q13/Q13.js
@@ -0,0 +1,12 @@
+let num = Number(prompt("enter an integer : "))
+while (num < 0 || isNaN(num)){
+    num = Number(prompt("Invalid integer, please try again : "))   
+}
+
+let sum = 0;
+let count = 1
+for (i = 1; i<= num; i++){
+    sum += i;
+    count++;
+}
+alert("total = "+ (sum/count))
